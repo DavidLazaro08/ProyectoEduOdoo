43c33e4d8cfb85e0941f9519364727d8ee99264b
David Gutiérrez <empirio14@alumnos.ilerna.com>
2026-02-16 00:45:57 +0100

    Retoques en manifest

--- addons/eduodoo/__manifest__.py
+++ addons/eduodoo/__manifest__.py
@@ -1,34 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "eduodoo",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
+    "name": "EduOdoo",
+    "summary": "Sistema integral de gestión para una academia de cursos",
+    "description": """
+EduOdoo es un módulo Odoo para gestionar una academia de cursos.
+Incluye la gestión de:
+- Cursos y convocatorias (sesiones)
+- Alumnos y matrículas
+- Profesores y asignación de sesiones
+- Facturas asociadas a matrículas
+- Vistas avanzadas (calendario y kanban) para planificación
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "author": "David Gutiérrez",
+    "website": "",
+    "category": "Education",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/views.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "installable": True,
+    "application": True,
 }
-
